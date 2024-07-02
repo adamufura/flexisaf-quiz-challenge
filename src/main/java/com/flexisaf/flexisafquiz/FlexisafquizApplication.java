de4c7a27d9e5546d451ed55c4fc069e2e3f22c08
adamufura <adamufura98@gmail.com>
2024-07-02 22:12:17 +0100

    added logging

--- src/main/java/com/flexisaf/flexisafquiz/FlexisafquizApplication.java
+++ src/main/java/com/flexisaf/flexisafquiz/FlexisafquizApplication.java
@@ -1,16 +1,19 @@
 package com.flexisaf.flexisafquiz;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlexisafquizApplication {
+    private static final Logger logger = LoggerFactory.getLogger(FlexisafquizApplication.class);
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(FlexisafquizApplication.class, args);
+        logger.debug("Debug log message");
+        logger.info("Info log message");
+        logger.error("Error log message");
 	}
 
 }
--- src/main/java/com/flexisaf/flexisafquiz/repository/SubjectRepository.java
+++ src/main/java/com/flexisaf/flexisafquiz/repository/SubjectRepository.java
@@ -2,7 +2,10 @@ package com.flexisaf.flexisafquiz.repository;
 
 import com.flexisaf.flexisafquiz.model.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.UUID;
 
+@Repository
 public interface SubjectRepository extends JpaRepository<Subject, String> {
 }
